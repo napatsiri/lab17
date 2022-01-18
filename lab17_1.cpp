7406c2c7f03bb24bf175de0daa4027ca2fd0077d
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2022-01-18 17:37:05 +0700

    lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,44 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *d,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << d[i];
+        if(i%M >= (M-1)) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *d,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        d[i] = (rand()%101);
+        d[i] /= 100;
+    }
+}
+
+void findRowSum(const double *d,double *s1,int N,int M){
+	for(int i = 0; i < N; i++){
+		double s = 0;
+		for(int j = 0; j < M; j++){
+			s += *((d+j)+(M*i));
+		}
+	s1[i] = s;
+	}
+}
+
+
+void findColSum(const double *d,double *s2,int N,int M){
+	int i = 0, j = 0;
+	s2[j] = 0;
+	for(int k = 0; k < M; k++){
+		i = k;
+		for(int v = 0; v < N; v++){
+			s2[j] += d[i];
+			i += M;
+		}
+	j++;
+	s2[j] = 0;
+	}
+}
